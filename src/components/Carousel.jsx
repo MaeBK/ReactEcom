43ab6ef0dae8206c6a0e8a335e57fdb00722d76c
Mae K <maeghanbk@gmail.com>
2022-07-27 09:54:16 -0400

    more added

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+`
+
+const Carousel = () => {
+  return (
+    <Container>
+        Carousel
+    </Container>
+  )
+}
+
+export default Carousel
--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -3,8 +3,11 @@ import styled from "styled-components"
 const Container = styled.div`
     height: 25px;
     background-color: #802BB1;
+    font-size: 15px
     color: white
     display: flex;
+    align-items: center;
+    justify-content:center;
     
 `
 
